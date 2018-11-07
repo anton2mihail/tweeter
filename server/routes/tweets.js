22d2e403df475959e517462f3ea08c0086eb8b7d
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-11-06 19:28:59 -0500

    Minor bug
    Fixed the returning of the tweet data

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -1,12 +1,11 @@
 "use strict";
 
-const userHelper    = require("../lib/util/user-helper")
+const userHelper = require("../lib/util/user-helper");
 
-const express       = require('express');
-const tweetsRoutes  = express.Router();
+const express = require("express");
+const tweetsRoutes = express.Router();
 
 module.exports = function(DataHelpers) {
-
   tweetsRoutes.get("/", function(req, res) {
     DataHelpers.getTweets((err, tweets) => {
       if (err) {
@@ -19,28 +18,29 @@ module.exports = function(DataHelpers) {
 
   tweetsRoutes.post("/", function(req, res) {
     if (!req.body.text) {
-      res.status(400).json({ error: 'invalid request: no data in POST body'});
+      res.status(400).json({ error: "invalid request: no data in POST body" });
       return;
     }
 
-    const user = req.body.user ? req.body.user : userHelper.generateRandomUser();
+    const user = req.body.user
+      ? req.body.user
+      : userHelper.generateRandomUser();
     const tweet = {
       user: user,
       content: {
-        text: req.body.text
+        text: req.body.text.match(/[^tweet=].*/g)[0]
       },
       created_at: Date.now()
     };
 
-    DataHelpers.saveTweet(tweet, (err) => {
+    DataHelpers.saveTweet(tweet, err => {
       if (err) {
         res.status(500).json({ error: err.message });
       } else {
-        res.status(201).send();
+        res.json(tweet);
       }
     });
   });
 
   return tweetsRoutes;
-
-}
+};
